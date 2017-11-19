db0bbe4364c7bc1b5db80c464faadcbaf8c5b76a
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-11-19 21:20:01 +0100

    export getProjection

--- graphql/Schema/Schema.js
+++ graphql/Schema/Schema.js
@@ -1,23 +1,11 @@
 import {typeDefs} from './typeDefs'
 import {resolvers} from './resolvers'
 import { makeExecutableSchema } from 'graphql-tools';
+import getProjection from './getProjection'
 
 export const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
 });
-
-/**
- * generate projection object for mongoose
- * @param  {Object} fieldASTs
- * @return {Project}
- */
-export function getProjection(fieldASTs) {
-    return fieldASTs.fieldNodes[0].selectionSet.selections.reduce((projections, selection) => {
-        projections[selection.name.value] = true;
-        return projections;
-    }, {});
-}
-
 
 export default schema;
