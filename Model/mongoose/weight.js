b835739fd887fb8a4888274cc3a7d74200ebd22c
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-06 11:39:08 +0100

    Add row students and meetings in schema

--- Model/mongoose/weight.js
+++ Model/mongoose/weight.js
@@ -7,6 +7,7 @@ var weightSchema = new Schema({
     value: Number,
     project_id: { type: String, ref: 'Project' },
     event_id: { type: String, ref: 'Event' },
+    students: [{ type: Schema.Types.ObjectId, ref: 'Student' }],
 }, { collection: "Weight" });
 
 var Weight = mongoose.model('Weight', weightSchema);
