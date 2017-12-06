5efcf3f08fa4e61acbe8202f7f3fa75792943223
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-06 14:55:39 +0100

    Create route & html for addUser

--- View/AddUser.vue
+++ View/AddUser.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
+      <div class="container">
       <h1>Ajouter un utilisateur</h1>
-      <form action="">
-
-      </form>
+        <form action="">
+            <div class="form-group">
+                <label for="name">Prénom et nom</label>
+                <input type="text" id="name" name="name" placeholder="Écrivez le prénom et nom" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Écrivez l'adresse mail" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" placeholder="Écrivez le mot de passe" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="company">Entreprise</label>
+                <input type="text" id="company" name="company" placeholder="Écrivez l'entreprise" class="form-control">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Créer un utilisateur</button>
+            </div>
+        </form>
+      </div>
   </div>
 </template>
 <script>
--- View/App.vue
+++ View/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
-        <h1>Skuuuurt</h1>
+        <h1>JiriSkuuuurt</h1>
+        <router-view></router-view>
     </div>
 </template>
 
