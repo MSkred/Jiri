fde1a10d046680d8576c9c29abbd3f84614c48c2
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-06 18:28:11 +0100

    Delete unused import

--- View/main.js
+++ View/main.js
@@ -2,15 +2,9 @@ import Vue from 'vue'
 import {store} from './store'
 import VueRouter from 'vue-router'
 import Routes from './routes'
-import ApolloClient from 'apollo-client'
-import { HttpLink, InMemoryCache } from 'apollo-client-preset'
-import VueApollo from 'vue-apollo'
-import fetch from 'node-fetch'
-
 import apolloProvider from './apollo'
-
 import App from './App.vue'
-// Init router
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
