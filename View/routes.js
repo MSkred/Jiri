eaab8181abf92dd2ee2eb460689f1c5eb0bc85ec
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-07 11:51:12 +0100

    Create login route and Auth controller

--- View/routes.js
+++ View/routes.js
@@ -1,9 +1,11 @@
 import AddUser from './components/AddUser.vue'
 import Home from './components/Home.vue'
 import AddStudent from './components/AddStudent.vue'
+import Login from './components/Login.vue'
 
 export default [
     { path: '/', component: Home, name: 'home' },
     { path: '/addUser', component: AddUser, name: 'addUser' },
     { path: '/addStudent', component: AddStudent, name: 'addStudent' },
+    { path: '/login', component: Login, name: 'login' },
 ]
