3fd863ce7d90315603b938efe30096f02def866c
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-10 14:26:35 +0100

    Created routes, view, mutation of project

---
+++ View/components/AddProject.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+      <div class="container">
+      <h1>Ajouter un projet</h1>
+            <div class="form-group">
+                <label for="name">Nom du projet</label>
+                <input v-model="name" type="text" id="name" name="name" placeholder="Écrivez le prénom et nom" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="description">Description du projet</label>
+                <textarea v-model="description" name="decription" id="description" cols="30" rows="10" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" @click="createProject">Créer un étudiant</button>
+            </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import VueApollo from 'vue-apollo'
+import nanoid from 'nanoid'
+
+import { CREATE_PROJECT_MUTATION } from '../constants/ProjectsCreate.gql'
+export default {
+  name: 'add-project',
+  data(){
+      return{
+          name: null,
+          description: null, 
+          soft_delete: false,
+      }
+  },
+  methods: {
+      createProject(){
+          const { name, description } = this;
+          this.$apollo.mutate({
+              mutation: CREATE_PROJECT_MUTATION,
+              variables: {
+                  name,
+                  description, 
+              },
+          }).then(data => {
+              console.log('Done project creation.');
+          }).catch(error => {
+              console.log('---Project creation failed' + error)
+          });
+      }
+  }
+}
+</script>
--- View/routes.js
+++ View/routes.js
@@ -1,11 +1,13 @@
-import AddUser from './components/AddUser.vue'
 import Home from './components/Home.vue'
+import AddUser from './components/AddUser.vue'
 import AddStudent from './components/AddStudent.vue'
+import AddProject from './components/AddProject.vue'
 import Login from './components/Login.vue'
 
 export default [
     { path: '/', component: Home, name: 'home' },
     { path: '/addUser', component: AddUser, name: 'addUser' },
     { path: '/addStudent', component: AddStudent, name: 'addStudent' },
+    { path: '/addProject', component: AddProject, name: 'addProject' },
     { path: '/login', component: Login, name: 'login' },
 ]
