4dfbbcf3114cb51990ec66848c3db31950d3a757
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-12 18:27:00 +0100

    Create form with stepByStep animation

--- View/components/AddEvent.vue
+++ View/components/AddEvent.vue
@@ -3,99 +3,123 @@
       <div class="container">
       <h1>Ajouter un événement</h1>
       
-            <!-- Name event -->
-            <div class="form-group">
-                <label for="courseName">Nom de l'événement</label>
-                <input v-model="courseName" type="text" id="courseName" name="courseName" placeholder="Écrivez le nom" class="form-control">
-            </div>
-            <!-- Year event -->
-            <div class="form-group">
-                <label for="academicYear">Sélectionnez l'année académique</label>
-                <select v-model="academicYear" name="academicYear" id="academicYear" class="form-control">
-                    <option value="2017">2017 - 2018</option>
-                    <option value="2018">2018 - 2019</option>
-                    <option value="2019">2019 - 2020</option>
-                    <option value="2020">2020 - 2021</option>
-                    <option value="2021">2021 - 2022</option>
-                </select>
-            </div>
-            <!-- Add & remove jury event -->
-            <div class="form-group">
-                <div>
-                    <label for="jury">Ajoutez des membres du jury</label>
-                    <ul>
-                        <li v-for="(jury, key) in jurys" :value="jury.id" :key="jury.id">
-                            {{jury.name}}
-                            <button type="submit" class="btn btn-primary" v-on:click="addJury(key)">+</button>
-                        </li>
-                    </ul>
-                </div>
-                <div v-if="eventJurys">
-                    <label for="jury">Membres du jury</label>
-                    <ul>
-                        <li v-for="(jury, key) in eventJurys" :value="jury.id" :key="jury.id">
-                            {{jury.name}}
-                            <button type="submit" class="btn btn-danger" v-on:click="removeJury(key)">-</button>
-                        </li>
-                    </ul>
-                </div>
-            </div> 
-            <!-- Add & remove student event -->
-            <div class="form-group">
-                <div>
-                    <label for="jury">Ajoutez des étudiants</label>
-                    <ul>
-                        <li v-for="(student, key) in students" :value="student.id" :key="student.id">
-                            {{student.name}}
-                            <button type="submit" class="btn btn-primary" v-on:click="addStudent(key)">+</button>
-                        </li>
-                    </ul>
-                </div>
-                <div v-if="eventStudents >= [0]">
-                    <label for="jury">Étudiant(es) ajouté à l'événement</label>
-                    <ul>
-                        <li v-for="(student, key) in eventStudents" :value="student.id" :key="student.id">
-                            {{student.name}}
-                            <button type="submit" class="btn btn-danger" v-on:click="removeStudent(key)">-</button>
-                        </li>
-                    </ul>
-                </div>
-            </div> 
-            <!-- Add & remove project event -->
-            <div class="form-group">
-                <label for="project">Sélectionnez les projets</label>
-                <label v-for="(project, key) in projects" :value="project.id" :key="project.id" class="form-check">
-                    <!--Add project -->
-                    <input v-if="!project.event" v-model="project.event" @click="addProject(key)" type="checkbox" class="form-check-input"></input>
-                    
-                    <!--Delete project -->
-                    <input v-if="project.event" v-model="project.event" @click="removeProject(key)" type="checkbox" class="form-check-input">{{project.name}}</input>
-                </label>
-            </div>
-            <!-- Validate event -->
-            <div class="form-group">
-                <li>
-                    Nom: {{courseName}}
+           <form id="msform">
+    <ul id="progressbar">
+        <li class="active">Nom & année</li>
+        <li>Ajout du jury</li>
+        <li>Ajout des étudiants</li>
+        <li>Ajout des projets</li>
+        <li>Validation des informations</li>
+    </ul>
+    <fieldset>
+        <h2 class="fs-title">Écrivez le nom et l'année de l'événement</h2>
+        <h3 class="fs-subtitle">Ceci est l'étape 1</h3>
+        <input v-model="courseName" type="text" id="courseName" name="courseName" placeholder="Écrivez le nom" class="form-control">
+        <label for="academicYear">Sélectionnez l'année académique</label>
+        <select v-model="academicYear" name="academicYear" id="academicYear" class="form-control">
+            <option value="2017">2017 - 2018</option>
+            <option value="2018">2018 - 2019</option>
+            <option value="2019">2019 - 2020</option>
+            <option value="2020">2020 - 2021</option>
+            <option value="2021">2021 - 2022</option>
+        </select>
+        <input type="button" name="next" class="next action-button" value="Étape suivante" />
+    </fieldset>
+    <fieldset>
+        <h2 class="fs-title">Ajoutez les membres du jury</h2>
+        <h3 class="fs-subtitle">Ceci est l'étape 2</h3>
+        <div>
+            <label for="jury">Ajoutez des membres du jury</label>
+            <ul>
+                <li v-for="(jury, key) in jurys" :value="jury.id" :key="jury.id">
+                    {{jury.name}}
+                    <button type="submit" class="btn btn-primary" v-on:click="addJury(key)">+</button>
                 </li>
-                <li>
-                    Année: {{academicYear}}
+            </ul>
+        </div>
+        <div v-if="eventJurys">
+            <label for="jury">Membres du jury</label>
+            <ul>
+                <li v-for="(jury, key) in eventJurys" :value="jury.id" :key="jury.id">
+                    {{jury.name}}
+                    <button type="submit" class="btn btn-danger" v-on:click="removeJury(key)">-</button>
                 </li>
-                <ul>Jurys:
-                    <li v-for="(jury, key) in eventJurys">
-                        {{jury.name}}
-                    </li>
-                    <li v-if="eventJurys <= [0]">Pas de jurys</li>
-                </ul>
-                <ul>Étudiants:
-                    <li v-for="(student, key) in eventStudents">
-                        {{student.name}}
-                    </li>
-                    <li v-if="eventStudents <= [0]">Pas d'étudiants</li>
-                </ul>
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary" @click="createEvent">Créer un événement</button>
-            </div>
+            </ul>
+        </div>
+        <input type="button" name="previous" class="previous action-button" value="Previous" />
+        <input type="button" name="next" class="next action-button" value="Next" />
+    </fieldset>
+    <fieldset>
+        <h2 class="fs-title">Ajoutez les étudiants</h2>
+        <h3 class="fs-subtitle">Ceci est l'étape 3</h3>
+        <div>
+            <label for="jury">Ajoutez des étudiants</label>
+            <ul>
+                <li v-for="(student, key) in students" :value="student.id" :key="student.id">
+                    {{student.name}}
+                    <button type="submit" class="btn btn-primary" v-on:click="addStudent(key)">+</button>
+                </li>
+            </ul>
+        </div>
+        <div v-if="eventStudents >= [0]">
+            <label for="jury">Étudiant(es) ajouté à l'événement</label>
+            <ul>
+                <li v-for="(student, key) in eventStudents" :value="student.id" :key="student.id">
+                    {{student.name}}
+                    <button type="submit" class="btn btn-danger" v-on:click="removeStudent(key)">-</button>
+                </li>
+            </ul>
+        </div>
+        <input type="button" name="previous" class="previous action-button" value="Previous" />
+        <input type="button" name="next" class="next action-button" value="Next" />
+    </fieldset>
+    <fieldset>
+        <h2 class="fs-title">Ajoutez les projets</h2>
+        <h3 class="fs-subtitle">Ceci est l'étape 4</h3>
+        <label for="project">Sélectionnez les projets</label>
+        <label v-for="(project, key) in projects" :value="project.id" :key="project.id" class="form-check">
+            <!--Add project -->
+            <input v-if="!project.event" v-model="project.event" @click="addProject(key)" type="checkbox" class="form-check-input"></input>
+        
+            <!--Delete project -->
+            <input v-if="project.event" v-model="project.event" @click="removeProject(key)" type="checkbox" class="form-check-input">{{project.name}}</input>
+        </label>
+        <input type="button" name="previous" class="previous action-button" value="Previous" />
+        <input type="button" name="next" class="next action-button" value="Next" />
+    </fieldset>
+    <fieldset>
+        <h2 class="fs-title">Validez les informations de l'événement</h2>
+        <h3 class="fs-subtitle">Ceci est la dernières étape</h3>
+        <div class="form-group">
+            <li>
+                Nom: {{courseName}}
+            </li>
+            <li>
+                Année: {{academicYear}}
+            </li>
+            <ul>Jurys:
+                <li v-for="(jury, key) in eventJurys">
+                    {{jury.name}}
+                </li>
+                <li v-if="eventJurys <= [0]">Pas de jurys</li>
+            </ul>
+            <ul>Étudiants:
+                <li v-for="(student, key) in eventStudents">
+                    {{student.name}}
+                </li>
+                <li v-if="eventStudents <= [0]">Pas d'étudiants</li>
+            </ul>
+            <ul>Projects:
+                <li v-for="(projet, key) in eventProjects">
+                    {{projet.name}}
+                </li>
+                <li v-if="eventProjects <= [0]">Pas de projets</li>
+            </ul>
+        </div>
+        <input type="button" name="previous" class="previous action-button" value="Previous" />
+        <input type="submit" name="submit" class="submit action-button" value="Submit" />
+    </fieldset>
+</form>
       </div>
   </div>
 </template>
