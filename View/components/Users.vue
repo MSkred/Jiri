7e626885c440be9c4459dc8b3fa57c86d7edb3f3
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-21 19:32:29 +0100

    Fixed users desactivation with modals confirmation

--- View/components/Users.vue
+++ View/components/Users.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="container">
-      <h1>Tous les utilisateurs</h1>
-      <div class="container__user user">
-          <router-link v-for="(user, key) in users" :to="{name: 'user', params: {id: user.id}}" :href="`/user/${user.id}`">
-            <div class="user__item" v-if="!user.softDelete">
-                <h2 class="title">{{user.name}}</h2>
-                <p class="year">{{user.company}}</p>
-                <p class="event">{{user.juryEvents.length}} événements</p>
-                <p class="meeting">{{user.meetings.length}} meetings</p>
-                <button @click.prevent="setDesactivate(user.id)">Désactiver</button>
-            </div>
-          </router-link>
-      </div>
-  </div>
+    <div class="container">
+        <h1>Tous les utilisateurs</h1>
+        <div class="container__user user">
+            <router-link v-for="(user, key) in users" :to="{name: 'user', params: {id: user.id}}" :href="`/user/${user.id}`">
+                <div class="user__item" v-if="!user.softDelete">
+                    <h2 class="title">{{user.name}}</h2>
+                    <p class="year">{{user.company}}</p>
+                    <p class="event">{{user.juryEvents.length}} événements</p>
+                    <p class="meeting">{{user.meetings.length}} meetings</p>
+                    <button id="show-modal" @click.prevent="showModal = true; setDesactivateData(user.id, user.name)">Désactiver</button>
+                </div>
+            </router-link>
+            <desactivate v-if="showModal" >
+                <h3 slot="header">Voullez-vous vraiment désactiver {{desactivateItem.name}}</h3>
+                <div slot="footer">
+                    <button @click.prevent="setDesactivate(desactivateItem.id)">Désactiver</button>
+                    <button @click.prevent="showModal = false">Annuler</button>
+                </div>
+            </desactivate>
+        </div>
+    </div>
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
+import Desactivate from './Desactivate.vue';
 export default {
     name: 'users',
+    components: {
+        Desactivate
+    },
+    data(){
+        return{
+            showModal: false,
+            desactivateItem: {
+                name: null,
+                id: null,
+            }
+        }
+    },
     computed: {
         ...mapGetters([
             'users',
@@ -27,7 +47,11 @@ export default {
     methods: {
         ...mapActions([
             'setDesactivate',
-        ])
+        ]),
+        setDesactivateData(id, name){
+            this.desactivateItem.id = id;
+            this.desactivateItem.name = name;
+        }
     },
     actions: {
         ...mapActions([
