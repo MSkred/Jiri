33ab25e902c09cd99a7f43cb1047d6d3abd040c3
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-21 16:36:49 +0100

    Created users view

---
+++ View/components/Users.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+      <h1>Tous les utilisateurs</h1>
+      <div class="container__user user">
+          <router-link v-for="(user, key) in users" :to="{name: 'user', params: {id: user.id}}" :href="`/user/${user.id}`">
+            <div class="user__item">
+                <h2 class="title">{{user.name}}</h2>
+                <p class="year">{{user.company}}</p>
+                <p class="event"> jurys</p>
+                <p class="meeting"> étudiants</p>
+            </div>
+          </router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+    name: 'users',
+    computed: {
+        ...mapGetters([
+            'users',
+        ])
+    },
+    created(){
+        // Users recuperation
+        this.$store.dispatch('setAllUsers')
+    }
+}
+</script>
