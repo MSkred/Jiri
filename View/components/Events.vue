13a2137675fb4fa07bb51e6b788622864ed30eb4
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-26 14:33:20 +0100

    Added events design v1

--- View/components/Events.vue
+++ View/components/Events.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="container">
-      <h1>Tous les événements</h1>
-      <div class="container__event event">
-          <router-link v-for="(event, key) in events" :to="{name: 'event', params: {id: event.id}}" :href="`/event/${event.id}`">
-            <div class="event__item">
-                <h2 class="title">{{event.courseName}}</h2>
-                <p class="year">{{event.academicYear}}</p>
-                <p class="jurys">{{event.jurys.length}} jurys</p>
-                <p class="students">{{event.students.length}} étudiants</p>
+    <div class="wrapper">
+        <section class="hero is-link">
+            <div class="hero-body">
+                <div class="container">
+                <h1 class="title">
+                    Tous les événements
+                </h1>
+                <h2 class="subtitle">
+                    Primary subtitle
+                </h2>
+                </div>
             </div>
-          </router-link>
+        </section>
+      <div class="papa">
+          <div v-for="(event, key) in events" class="enfant">
+            <router-link :to="{name: 'event', params: {id: event.id}}" :href="`/event/${event.id}`" class="contenu">
+                <div class="flex" v-if="!event.softDelete">
+                    <div class="flex__header">
+                        <h2 class="title">{{event.courseName}}</h2>
+                        <p class="subtitle">{{event.academicYear}}</p>
+                    </div>
+                    <div class="flex__content">
+                        <p class="jurys">{{event.jurys.length}} jurys</p>
+                        <p class="students">{{event.students.length}} étudiants</p>
+                    </div>
+                    <div class="flex__footer">
+                        <button class="modify"  @click.prevent="showModifyModal = true; setModifyData(event)">Modifier</button>
+                        <button id="show-modal" @click.prevent="showDesactivateModal = true; setDesactivateData(event)">Désactiver</button>
+                    </div>
+                </div>
+            </router-link>
+          </div>
       </div>
   </div>
 </template>
