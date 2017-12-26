d336a65951a41069bfa1a6eea903d3650e41f73f
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-26 13:43:27 +0100

    Students page design v1

--- View/components/Students.vue
+++ View/components/Students.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="container">
-      <h1>Tous les étudiants</h1>
-      <div class="container__student student">
-          <router-link v-for="(student, key) in allStudents" :to="{name: 'student', params: {id: student.id}}" :href="`/student/${student.id}`">
-            <div class="student__item">
-                <h2 class="title">{{student.name}}</h2>
-                <p class="year">{{student.email}}</p>
-                <p class="event">{{student.studentEvents.length}} événements</p>
-                <p class="meeting">{{student.implementations.length}} implémentations</p>
+    <div class="wrapper">
+        <section class="hero is-link">
+            <div class="hero-body">
+                <div class="container">
+                <h1 class="title">
+                    Tous les étudiants
+                </h1>
+                <h2 class="subtitle">
+                    Primary subtitle
+                </h2>
+                </div>
             </div>
-          </router-link>
+        </section>
+      <div class="papa">
+          <div class="enfant" v-for="(student, key) in allStudents" >
+            <router-link class="contenu" :to="{name: 'student', params: {id: student.id}}" :href="`/student/${student.id}`">
+                <div class="flex" v-if="!student.softDelete">
+                    <div class="flex__header">
+                        <h2 class="title">{{student.name}}</h2>
+                        <p class="subtitle">{{student.email}}</p>
+                    </div>
+                    <div class="flex__content">
+                        <p class="event">{{student.studentEvents.length}} événements</p>
+                        <p class="meeting">{{student.implementations.length}} implémentations</p>
+                    </div>
+                    <div class="flex__footer">
+                        <button class="modify"  @click.prevent="showModifyModal = true; setModifyData(user)">Modifier</button>
+                        <button id="show-modal" @click.prevent="showDesactivateModal = true; setDesactivateData(user)">Désactiver</button>
+                    </div>
+                </div>
+            </router-link>
+          </div>
       </div>
   </div>
 </template>
