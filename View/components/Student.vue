7436582e6b8253b18a9d3ece65c6a1047239248a
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-12-30 14:54:11 +0100

    Display event & implementation of student

--- View/components/Student.vue
+++ View/components/Student.vue
@@ -1,12 +1,47 @@
 <template>
-    <div class="container">
-        <h1>{{student.name}}</h1>
-        <p>{{student.company}}</p>
-        <div class="container__jurys jurys">
-            <h2>Les événements de {{student.name}}</h2>
+    <div class="wrapper">
+        <section class="hero is-link">
+            <div class="hero-body">
+                <div class="container">
+                <h1 class="title">
+                    {{student.name}}
+                </h1>
+                <h2 class="subtitle">
+                    {{student.company}}
+                </h2>
+                </div>
+            </div>
+        </section>
+        <h2>Les événements de {{student.name}}</h2>
+        <div class="papa">
+            <div v-for="(event, key) in student.studentEvents" class="enfant">
+                <router-link :to="{name: 'event', params: {id: event.id}}" :href="`/event/${event.id}`" class="contenu">
+                    <div class="flex" v-if="!event.softDelete">
+                        <div class="flex__header">
+                            <h2 class="title">{{event.courseName}}</h2>
+                            <p class="subtitle">{{event.academicYear}}</p>
+                        </div>
+                        <div class="flex__content">
+                        </div>
+                    </div>
+                </router-link>
+            </div>
         </div>
-        <div class="container__students students">
-            <h2>Les implémentations de {{student.name}}</h2>
+        <h2>Les implémentations de {{student.name}}</h2>
+        <div class="papa">
+            <div v-for="(implementation, key) in student.implementations" class="enfant">
+                <div class="contenu">
+                    <div class="flex">
+                        <div class="flex__header">
+                            <h2 class="title">{{implementation.project.name}}</h2>
+                            <p class="subtitle">{{implementation.project.weight}}</p>
+                        </div>
+                        <div class="flex__content">
+                            <p>{{implementation.project.description}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
