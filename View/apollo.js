54e04c05be3e2c3a2d0e931329209710d9c7d054
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-02 17:33:48 +0100

    Update apollo.js and add module for subscription

--- View/apollo.js
+++ View/apollo.js
@@ -1,34 +1,47 @@
 import Vue from 'vue'
 import ApolloClient from 'apollo-client'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import { HttpLink, InMemoryCache } from 'apollo-client-preset'
+import { getOperationAST } from 'graphql'
+import { WebSocketLink } from 'apollo-link-ws'
 import { ApolloLink } from 'apollo-link'
 import VueApollo from 'vue-apollo'
-import fetch from 'node-fetch'
 
 
-const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjazgfjex0fer0113x18k2m4z' });
-
-const middlewareLink = new ApolloLink((operation, forward) => {
-    operation.setContext({
-        headers: {
-            authorization: `Bearer ${localStorage.getItem('userToken')}`
+const link = ApolloLink.split(
+    (operation) => {
+        const operationAST = getOperationAST(operation.query, operation.operationName);
+        operation.setContext({
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('userToken')}`
+            }
+        });
+        return !!operationAST && operationAST.operation === 'subscription';
+    },
+    new SubscriptionClient('wss://subscriptions.graph.cool/v1/cjazgfjex0fer0113x18k2m4z', {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem('userToken')
         }
-    });
-    return forward(operation);
-});
+    }),
+    new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjazgfjex0fer0113x18k2m4z' }),
+);
 
 
+// Create cache
+const cache = new InMemoryCache();
 
-// Create the apollo client
+// Init ApolloClient with link and cache
 export const apolloClient = new ApolloClient({
-    link: middlewareLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    link,
+    cache,
     connectToDevTools: true,
 });
 
+// Use apollo in Vue.js
 Vue.use(VueApollo)
 
+// Export client
 export const apolloProvider = new VueApollo({
     defaultClient: apolloClient,
 })
-
