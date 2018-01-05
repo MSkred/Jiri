3c929057c68b7e5f82de3bdebcff923e613da186
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-05 12:16:24 +0100

    Installed and imported KeenUI

--- View/main.js
+++ View/main.js
@@ -6,15 +6,21 @@ import { store } from './store'
 
 // Import french errors messages
 import fr from 'vee-validate/dist/locale/fr';
+
 // Import VeeValidate modul
 import VeeValidate, { Validator } from 'vee-validate';
 import App from './App.vue'
+
+// Import KeenUI
+import KeenUI from 'keen-ui';
+import 'keen-ui/dist/keen-ui.css';
+
 
 // Merge the locales.
 Validator.localize('fr', fr);
 
 // Use this
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, KeenUI)
 
 
 new Vue({
@@ -23,4 +29,7 @@ new Vue({
     apolloProvider,
     render: h => h(App),
     router,
+    components: {
+        UiAlert,
+    }
 })
