4873e5a47dfb13c7f21c248954600e28b503892b
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-05 11:19:50 +0100

    Added createUser errors management

--- View/components/AddUser.vue
+++ View/components/AddUser.vue
@@ -2,27 +2,38 @@
   <div>
       <div class="container">
       <h1>Ajouter un utilisateur</h1>
-            <div class="form-group">
+      <form @submit.prevent="validateBeforeSubmit">
+            <div class="form-group" :class="{ 'control': true }">
                 <label for="name">Prénom et nom</label>
-                <input v-model="name" type="text" id="name" name="name" placeholder="Écrivez le prénom et nom" class="form-control">
+                <input v-model="name" type="text" id="name" name="name" placeholder="Écrivez le prénom et nom" class="form-control"
+                data-vv-as="Le champs prénom et nom" v-validate="'required|alpha_spaces|min:5'" :class="{'input': true, 'is-danger': errors.has('name') }">
+                <span v-show="errors.has('name')" class="help is-danger">{{ errors.first('name') }}</span>
             </div>
             <div class="form-group" :class="{ 'control': true }">
                 <label for="email">Email</label>
                 <input v-model="email" type="email" id="email" name="email" placeholder="Écrivez l'adresse mail" class="form-control"
-                v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }">
+                data-vv-as="Le champs email" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }">
                 <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
             </div>
             <div class="form-group">
                 <label for="password">Mot de passe</label>
-                <input v-model="password" type="password" id="password" name="password" placeholder="Écrivez le mot de passe" class="form-control">
+                <input type="password" id="password" name="password" placeholder="Écrivez le mot de passe" class="form-control"
+                data-vv-as="Le champs mot de passe" v-validate="'required|confirmed:c_password|min:4'" :class="{'input': true, 'is-danger': errors.has('password') }">
+                <span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>
+            
+                <label for="c_password">Comfirmez le mot de passe</label>
+                <input v-model="password" type="password" name="c_password" placeholder="Confirmez le mot de passe" class="form-control" data-vv-as="'Confirmez le mot de passe'" v-validate="'required|confirmed:password|min:4'" :class="{'input': true, 'is-danger': errors.has('password') }">
             </div>
             <div class="form-group">
                 <label for="company">Entreprise</label>
-                <input v-model="company" type="text" id="company" name="company" placeholder="Écrivez l'entreprise" class="form-control">
+                <input v-model="company" type="text" id="company" name="company" placeholder="Écrivez l'entreprise" class="form-control"
+                data-vv-as="Le champs entreprise" v-validate="'required|alpha_num|min:2'" :class="{'input': true, 'is-danger': errors.has('company') }">
+                <span v-show="errors.has('company')" class="help is-danger">{{ errors.first('company') }}</span>
             </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary" @click.prevent="createUser">Créer un utilisateur</button>
+            <div class="control">
+                <button type="submit" class="button is-primary">Créer un utilisateur</button>
             </div>
+      </form>
       </div>
   </div>
 </template>
@@ -45,6 +56,18 @@ export default {
         createUser() {
             let {email, password, name, company, isAdmin, softDelete} = this;
             Bus.$emit('createUser', {email, password, name, company, isAdmin, softDelete});
+        },
+        validateBeforeSubmit() {
+            this.$validator.validateAll().then((result) => {
+                if (result) {
+                    // eslint-disable-next-line
+                    alert('From Submitted!');
+                    this.createUser();
+                    return;
+                }
+
+                alert('Corrigez les erreurs');
+            });
         }
     },
 };
--- View/main.js
+++ View/main.js
@@ -4,14 +4,15 @@ import router from './router'
 import {apolloProvider} from './apollo'
 import { store } from './store'
 
-
+// Import french errors messages
 import fr from 'vee-validate/dist/locale/fr';
+// Import VeeValidate modul
 import VeeValidate, { Validator } from 'vee-validate';
 import App from './App.vue'
 
 // Merge the locales.
 Validator.localize('fr', fr);
-
+// Use this
 Vue.use(VeeValidate)
 
 
