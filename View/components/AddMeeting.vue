ba682c9d30076ef15ba736f81a421c0d79c4ce9a
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-06 12:23:22 +0100

    Designed createMeeting form

--- View/components/AddMeeting.vue
+++ View/components/AddMeeting.vue
@@ -1,31 +1,16 @@
 <template>
     <div class="container">
         <h1>Créer un meeting</h1>
-        <form id="msform">
-            <fieldset>
-                <h2 class="fs-title">Choisissez un étudiant</h2>
-                <h3 class="fs-subtitle">Ceci est l'étape 1</h3>
+        <form>
+            <md-field>
                 <label for="students">Sélectionnez un étudiant</label>
-                <select name="students" id="students" v-model="studentId">
-                    <option value="null">Aucun étudiant sélectionné</option>
-                    <option v-for="(student, key) in event.students" :value="student.id">{{student.name}}</option>
-                </select>
-            </fieldset>
-            <!-- <fieldset>
-                <h2 class="fs-title">Ajoutez les projets</h2>
-                <h3 class="fs-subtitle">Ceci est l'étape 2</h3>
-                <label for="project">Sélectionnez les projets</label>
-                <label v-for="(project, key) in event.projects" :key="project.id" class="form-check">
-                    Add project
-                    <input v-if="!project.meeting" @click.prevent="addProjectToMeeting(key)" v-model="project.meeting" type="checkbox" class="form-check-input"></input>
-                    
-                   Delete project
-                    <input v-if="project.meeting" @click.prevent="removeProjectToMeeting(key)" v-model="project.meeting" type="checkbox" class="form-check-input">{{project.name}}</input>
-                </label>
-                <input type="button" name="previous" class="previous action-button" value="Previous" />
-                <input type="button" name="next" class="next action-button" value="Next" />
-            </fieldset> -->
-            <button @click.prevent="startMeeting">Commencez le meeting</button>
+                <md-select name="students" id="students" v-model="studentId">
+                    <md-option v-for="(student, key) in event.students" :value="student.id">{{student.name}}</md-option>
+                </md-select>
+            </md-field>
+            <md-button @click.prevent="startMeeting" class="md-raised md-primary">
+                Commencez le meeting
+            </md-button>
         </form>
     </div>
 </template>
