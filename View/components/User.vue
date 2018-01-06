b378ab549b28ab757fb838f2c168503591763e4d
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-06 13:58:08 +0100

    Added tabs on single user

--- View/components/User.vue
+++ View/components/User.vue
@@ -1,13 +1,59 @@
 <template>
-    <div class="container">
-        <h1>{{user.name}}</h1>
-        <p>{{user.company}}</p>
-        <div class="container__jurys jurys">
-            <h2>Les événements de {{user.name}}</h2>
-        </div>
-        <div class="container__students students">
-            <h2>Les meetings de {{user.name}}</h2>
-        </div>
+    <div class="wrapper">
+        <section class="hero is-link">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        {{user.name}}
+                    </h1>
+                    <h2 class="subtitle">
+                        {{user.company}}
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <md-tabs>
+            <md-tab md-label="événements">
+                <div class="md-layout papa">
+                    <div v-for="(event, key) in user.juryEvents" class="enfant">
+                        <router-link :to="{name: 'event', params: {id: event.id}}" :href="`/event/${event.id}`" class="contenu">
+                            <md-card class="md-primary" md-with-hover>
+                                <md-ripple>
+                                    <md-card-header>
+                                        <div class="md-title">{{event.courseName}}</div>
+                                        <div class="md-subhead">{{event.academicYear}}</div>
+                                    </md-card-header>
+                                </md-ripple>
+                            </md-card>
+                        </router-link>
+                    </div>
+                </div>
+            </md-tab>
+            <md-tab md-label="meetings">
+                <div class="md-layout papa">
+                    <div v-for="(meeting, key) in user.meetings" class="enfant">
+                        <div class="contenu">
+                            <md-card class="md-primary" md-with-hover>
+                                <md-ripple>
+                                    <md-card-header>
+                                        <div class="md-title">{{meeting.student.name}}</div>
+                                        <div class="md-subhead">Côte globale de {{meeting.evaluation}}/20</div>
+                                    </md-card-header>
+                                    <md-card-content>
+                                        <p>{{meeting.event.courseName}} de {{meeting.event.academicYear}}</p>
+                                        <p>{{meeting.comment}}</p>
+                                    </md-card-content>
+                                    <md-card-actions>
+                                        <md-button @click.prevent="showMeetingModal = true; setModifyData(meeting)">Voir le meeting</md-button>
+                                        <md-button @click.prevent="showModifyModal = true; setModifyData(meeting)">Modifier</md-button>
+                                    </md-card-actions>
+                                </md-ripple>
+                            </md-card>
+                        </div>
+                    </div>
+                </div>
+            </md-tab>
+        </md-tabs>
     </div>
 </template>
 
