d603717ee8c6b18e72b9ccac5ca827b745c6b225
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-09 20:38:59 +0100

    Reset last nav menu

--- View/components/Navigation.vue
+++ View/components/Navigation.vue
@@ -1,59 +1,40 @@
 <template>
-    <nav class="navbar is-transparent">
-      <div class="navbar-brand">
-        <router-link :to="{name :'home'}" class="navbar-item">Jiri</router-link>
-        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-
-      <div id="navbarExampleTransparentExample" class="navbar-menu" v-if="userId">
-        <div class="navbar-start" v-if="currentUser.isAdmin">
+  <nav class="navbar is-link" role="navigation" aria-label="dropdown navigation">
+      <template v-if="userId && currentUser.isAdmin">
+        <div class="navbar-start">
           <router-link :to="{name :'home'}" class="navbar-item">Dashboard</router-link>
           <div class="navbar-item has-dropdown is-hoverable">
             <router-link :to="{name :'users'}" class="navbar-link">Tous les utilisateurs</router-link>
-            <div class="navbar-dropdown is-boxed">
+            <div class="navbar-dropdown">
               <router-link :to="{name :'addUser'}" class="navbar-item">Ajouter un utilisateur</router-link>
             </div>
           </div>
           <div class="navbar-item has-dropdown is-hoverable">
             <router-link :to="{name :'students'}" class="navbar-link">Tous les étudiants</router-link>
-            <div class="navbar-dropdown is-boxed">
+            <div class="navbar-dropdown">
               <router-link :to="{name :'addStudent'}" class="navbar-item">Ajouter un étudiant</router-link>
             </div>
           </div>
           <div class="navbar-item has-dropdown is-hoverable">
             <router-link :to="{name :'events'}" class="navbar-link">Tous les événements</router-link>
-            <div class="navbar-dropdown is-boxed">
+            <div class="navbar-dropdown">
               <router-link :to="{name :'addEvent'}" class="navbar-item">Ajouter un événement</router-link>
             </div>
           </div>
           <div class="navbar-item has-dropdown is-hoverable">
               <router-link :to="{name :'projects'}" class="navbar-link">Tous les projets</router-link>
-            <div class="navbar-dropdown is-boxed">
+            <div class="navbar-dropdown">
               <router-link :to="{name :'addProject'}" class="navbar-item">Ajouter un projet</router-link>
             </div>
           </div>
         </div>
-
         <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a href="javascript:avoid" @click.prevent="logout()"  class="navbar-item">Se deconecter</a>
-              </p>
-              <p class="control">
-                <router-link v-if="!userId" :to="{name :'login'}" class="navbar-item">Se connecter</router-link>
-              </p>
-            </div>
-          </div>
+          <a href="javascript:avoid" @click.prevent="logout()"  class="navbar-item">Se deconecter</a>
+          <router-link v-if="!userId" :to="{name :'login'}" class="navbar-item">Se connecter</router-link>
         </div>
-      </div>
-    </nav>
+      </template>
+  </nav>
 </template>
-
 
 <script>
 import {mapGetters} from 'vuex'
